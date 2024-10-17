3f7ec0450e610ad9cd910d55deea186c93031dda
Iko1703 <[idmiko@yandex.ru]>
2024-10-17 12:37:27 +0300

    v0.3 - transferring to another computer 2

--- src/PasswordGenerator.java
+++ src/PasswordGenerator.java
@@ -1,4 +1,45 @@
-public class PasswordGenerator{
+import java.util.HashMap;
+import java.util.Map;
 
-    
+public class PasswordGenerator
+{
+    public static void main()
+    {
+        Map<String,String> numbers = new HashMap <String,String>() {
+            {
+                put("48","9");
+            }
+        };
+        Map<String,String> digit = new HashMap <String,String>(){
+            {
+                put("33","14");
+                put("58","6");
+                put("91","5");
+                put("123","3");
+            }
+        };
+        Map<String,String> letterUp = new HashMap <String,String>(){  
+            {
+                put("65","25");
+            }
+        };
+        Map<String,String> letterDown = new HashMap <String,String>(){
+            {
+                put("97","25");
+            }
+        };
+
+        
+
+        digit.put("33","14");
+
+
+        for (int i = 0; i <= 25; i++) 
+        {
+            int decimalCodePoint = 97 + i;
+            char[] charArray = Character.toChars(decimalCodePoint);
+            String digit = new String(charArray);
+            System.out.println("Код Unicode: " + Integer.toHexString(decimalCodePoint) + "  Символ: " + digit);
+        }
+    }
 }
